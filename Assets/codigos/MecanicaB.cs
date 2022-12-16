7ef4831abaedc339edc0177f5c2ad8c69ac81b52
RonnieRadke17 <alexisemmanuelzombie@gmail.com>
2022-12-16 01:03:47 -0600

    prototipo controles y agrupacion elementos graficos
    prototipo de controles mejorados de personaje, agrupacion de varios elementos del laboratorio

---
+++ Assets/codigos/MecanicaB.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MecanicaB : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float speed;
+    public float maxSpeed;
+    public GameObject referencia;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+   
+    void FixedUpdate()
+    {
+        float moverHorizontal = Input.GetAxis("Horizontal");
+        float moverVertical = Input.GetAxis("Vertical");
+        
+            if(rb.velocity.magnitude > maxSpeed){
+                rb.velocity = rb.velocity.normalized * maxSpeed;
+            }
+
+        rb.AddForce(moverVertical * referencia.transform.forward * speed);
+        rb.AddForce(moverHorizontal * referencia.transform.right * speed);
+        
+    }
+}
