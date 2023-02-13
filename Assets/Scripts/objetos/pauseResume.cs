475df6ee63602a225b4365328970fd158ff89397
RonnieRadke17 <alexisemmanuelzombie@gmail.com>
2023-02-12 20:50:26 -0600

    Implementacion del menu de pausa
    más aparte más menus los cuales son necesarios pero los cuales aún no son funcionales

---
+++ Assets/Scripts/objetos/pauseResume.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseResume : MonoBehaviour
+{
+    //public GameObject pausePanel;
+    public void mostrar()
+    {
+       Debug.Log("saliendo");
+    }
+
+
+   /* public void pause(){
+        pausePanel.SetActive(true);
+        Time.timeScale = 0;
+        //Cursor.lookState = CursorLockMode.None;
+    }
+
+    public void resume(){
+        pausePanel.SetActive(false);
+        Time.timeScale = 1;
+        //Cursor.lookState = CursorLockMode.Locked;
+    }
+*/
+}
